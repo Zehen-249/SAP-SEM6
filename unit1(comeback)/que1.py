7c3a7444fdd464d1f3224974cecc3daecb40655a
Zehen-249 <1hasanmehendi123@gmail.com>
2025-04-19 19:24:32 +0530

    unit 1 and unit 2 first que

---
+++ unit1(comeback)/que1.py
@@ -0,0 +1,37 @@
+"""
+@copyright Riya pandey (thoda sa)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm, poisson, binom
+
+def plot_distribution_with_clt(N: int, M: int, plot_num: list, dist: str = 'normal', param: tuple = None):
+    if param :
+        if dist == 'normal':
+            data = np.random.normal(loc=param[0], scale=param[1], size=(N, M))
+        elif dist == "poisson":
+            data = np.random.poisson(lam=param[0], size=(N, M))
+        elif dist == "binomial":
+            data = np.random.binomial(n=param[0], p=param[1], size=(N, M))
+        elif dist =="cauchy":
+            data = np.random.standard_cauchy(size=(N, M)) 
+        
+        mean_vect = data.mean(axis=0)
+        mean_data = mean_vect.mean()
+        std_data = mean_vect.std()
+
+        
+        axs[plot_num[0],plot_num[1]].hist(mean_vect, bins=30, density=True, color='blue', edgecolor='k', label=dist)
+        x = np.linspace(min(mean_vect), max(mean_vect), 100)
+        axs[plot_num[0],plot_num[1]].plot(x, norm.pdf(x, mean_data, std_data), 'r--', label='Normal Fit')
+        axs[plot_num[0],plot_num[1]].set_title(f"{dist} Distribution")
+        axs[plot_num[0],plot_num[1]].legend()
+
+fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+plot_distribution_with_clt(N=10, M=10000,plot_num=[0,0], dist="normal", param=(50,0.5)) 
+plot_distribution_with_clt(N=10, M=10000,plot_num=[0,1], dist="poisson", param=(50,)) 
+plot_distribution_with_clt(N=10, M=10000,plot_num=[1,0], dist="binomial", param=(10,0.2)) 
+plot_distribution_with_clt(N=10, M=100,plot_num=[1,1], dist="cauchy", param=(10,)) 
+plt.tight_layout()
+plt.show()
---
+++ unit1(comeback)/que2.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def coutFreq(pair,X_,Y_):
+    x=pair[0]
+    y = pair[1]
+    count = 0
+
+    for i in range(len(X_)):
+        if X_[i]==x and Y_[i]==y:
+            count+=1
+    return count
+
+def print_table(data):
+
+    rows = sorted(set(k[0] for k in data.keys()))
+    cols = sorted(set(k[1] for k in data.keys()))
+
+    print("X|Y", end="")  
+    for col in cols:
+        print(f"{col:5}", end="")  
+    print()
+
+    for row in rows:
+        print(f"{row:2} ", end="")  
+        for col in cols:
+            print(f"{data.get((row, col), 0):5}", end="")  
+        print()
+
+def createDF(data):
+    rows = sorted(set(k[0] for k in data.keys()))
+    cols = sorted(set(k[1] for k in data.keys()))
+
+    df = pd.DataFrame(index=rows, columns=cols)
+
+    for (x, y), freq in data.items():
+        df.loc[x, y] = freq
+
+    return df
+
+N=1000 
+# X_real = np.random.randint(0,50,N)
+# Y_real = np.random.randint(50,100,N)
+
+X_real = np.random.poisson(lam = 5,size = N)
+Y_real = np.random.binomial(p=0.5,n=10,size =N)
+data = {}
+for i in X_real:
+
+    for j in Y_real:
+        data[(i,j)] = 0
+
+# for i,j in data.items():
+#     print(i,j)
+
+for pair,freq in data.items():
+    data[pair] = coutFreq(pair,X_real,Y_real)/N
+    # print(f"{pair} | {data[pair]}")
+
+    
+# print_table(data)
+df_real = createDF(data)
+# df_real["marginal Prob of X"] = df_real.sum(axis=1)
+# df_real.loc["marginal Prob of Y"] = df_real.sum(axis=0)
+print(df_real)
+
+df_real = df_real.apply(pd.to_numeric)
+
+
+plt.figure(figsize=(6, 5))  
+sns.heatmap(df_real, annot=True, cmap="coolwarm", fmt=".3f", linewidths=0)
+
+
+plt.xlabel("Y-Axis Values")
+plt.ylabel("X-Axis Values")
+plt.title("Heatmap of Data")
+
+
+plt.show()
